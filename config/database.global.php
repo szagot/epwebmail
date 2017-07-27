02f6afac3362bfaacecdb662fa3b78889896292f
Daniel Bispo <szagot@gmail.com>
2017-07-26 21:08:00 -0300

    Commit Inicial: copiado projeto em branco

---
+++ config/database.global.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Configuração de conexão ao BD
+ */
+
+return [
+    'driver'        => 'pdo_mysql',
+    'charset '      => 'utf8',
+    'driverOptions' => [
+        1002 => "SET NAMES 'UTF8'",
+    ],
+    'dbname'        => 'empty',
+    'host'          => 'localhost',
+    'user'          => 'user',
+    'password'      => 'pass',
+];
